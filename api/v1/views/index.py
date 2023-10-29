db8f309984336c4833081db597e83fa0bac5991b
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 15:36:00 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""New route"""
-from flask import Flask, jsonify
-from . import app_views
+"""Return status ok"""
+from api.v1.views import app_views
+from flask import jsonify  # convert to JSON
+# imports for task 5
 from models import storage
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Return the status of our api"""
-    dic = {'status': 'OK'}
-    return jsonify(dic)
+@app_views.route("/status", methods=['GET'])
+def return_status():
+    """Return status of GET request"""
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """Return the number of objects"""
-    d = {"amenities": storage.count(Amenity),
-         "cities": storage.count(City),
-         "places": storage.count(Place),
-         "reviews": storage.count(Review),
-         "states": storage.count(State),
-         "users": storage.count(User)}
-    return jsonify(d)
+@app_views.route('/stats')
+def return_count():
+    ''' Retrieve the number of each object by type '''
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
