1c669cf886f4a688c8779e4cff86463c49f1cf55
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-29 15:36:34 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-"""Flask web application"""
-from flask import Flask, make_response, jsonify
+''' Return the status of an API '''
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv  # to use environmental variables
+from flask import jsonify
+from werkzeug.exceptions import HTTPException  # to use errorhandler
 from flask_cors import CORS
 
 
+# instance app variable from Flask class
 app = Flask(__name__)
+# register the blueprint app_views for use
 app.register_blueprint(app_views)
-
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}},
-            methods=['GET', 'PUT', 'DELETE', 'POST'])
+# Cors(origin resource shering):https://flask-cors.readthedocs.io/en/latest/
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close(exception=None):
-    """Method close"""
+def teardown(self):
+    ''' Remove the current SQLAlchemy Session '''
     storage.close()
 
 
-@app.errorhandler(404)
-def error_404(error):
-    """Return json with 404 error"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+@app.errorhandler(HTTPException)
+def handle_exception(error):
+    ''' Use errorhandler to display 404 error page '''
+    return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    host = None
-    port = None
-    if os.getenv('HBNB_API_HOST'):
-        host = os.getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if os.getenv('HBNB_API_PORT'):
-        port = int(os.getenv('HBNB_API_PORT'))
-    else:
-        port = 5000
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    # return env variable if it exists
+    # otherwise return second argument
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True, debug=True)
